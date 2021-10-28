6182e2beda101e791c1233baecca2a82a31e17d8
ytw418 <ytw418@gmail.com>
2021-10-28 16:39:37 +0900

    푸터 모바일 이미지 추가 , 서브메뉴 높이 변경

---
+++ menu/menu.js
@@ -0,0 +1,68 @@
+$(document).on("scroll", function() {
+
+	if($(document).scrollTop()>100) {
+		$("header").removeClass("large").addClass("small");
+	} else {
+		$("header").removeClass("small").addClass("large");
+	}
+	
+});
+
+(function($){
+  $.fn.dropdown = function(){
+    return this.each(function(){
+      var $gnb = $(this),
+          $menu = $gnb.find(".menu"),
+          $depth1 = $gnb.find(".depth1"),
+          $depth2 = $gnb.find(".depth2");
+      
+      $gnb.mouseenter(function(){
+        gnbOn();
+      }).mouseleave(function(){
+        gnbOff();
+      });
+      
+      $gnb.find("a").focusin(function(){
+        gnbOn();
+      }).focusout(function(){
+        gnbOff();
+      });
+        
+      function gnbOn(){
+        $gnb.find($depth2).stop().animate({height: "230"});/*서브메뉴 높이 조절*/
+      }
+      
+      function gnbOff(){
+        $gnb.find($depth2).stop().animate({height: "0"});
+      }
+
+    });
+  }
+})(jQuery);
+
+if (matchMedia("screen and (min-width: 1024px)").matches) {
+  $(function(){
+  $(".dropdown").dropdown();
+});
+} else {
+ $(function(){
+});
+}
+
+/*전체메뉴*/
+
+$('#gnb').hover(function() {
+   $('#nav-icon1').toggleClass('nav-icon2');
+   $('#logo').toggleClass('logo2');
+  });
+
+/*$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#gnb').toggleClass('open');
+  });
+  
+$('.dropdown').hover(function() {
+   $('aside.util').toggleClass('util_on');
+  });*/
+
+
